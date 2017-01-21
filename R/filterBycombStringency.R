90d2786c18c98b5555935775bf085d08afdf17f5
Julaiti Shayiding <j.shahidin@gmail.com>
2017-01-21 18:22:51 +0100

    Revised implementation on filterBycombStringency is updated.

--- R/filterBycombStringency.R
+++ R/filterBycombStringency.R
@@ -15,11 +15,11 @@
 #' Passed to \link{FDR_stats}
 #' ERs that failed from Fisher method also passed to \link{mergeDiscERs}
 #'
-#' @param ERs output of \link{denoise_ERs},
+#' @param peakset output of \link{denoise_ERs},
 #' set of enriched regions stored in GRanges,
 #' where background signal won't be further evaluated.
 #'
-#' @param .hitList output of \link{filterByOverlapHit},
+#' @param ovHit output of \link{filterByOverlapHit},
 #' take \code{isSuffOverlap} as \code{TRUE}, only all enriched regions
 #' that comply minimum overlapping peak requirements are further evaluated.
 #'
@@ -28,7 +28,7 @@
 #' whether ERs fulfill stringency test,
 #' and result can be set of confirmed ERs, and discarded ERs respectively.
 #'
-#' @param isFisherPass logical vector that check whether
+#' @param isConfirmed logical vector that check whether
 #' all enriched regins are passed in Fisher' combined method.
 #'
 #' \code{TRUE}: return all enriched regions that fulfill Fisher's combined test,
@@ -38,10 +38,10 @@
 #' classified as \code{discardedERs} in \link[GenomicRanges]{GRangesList}
 #'
 #' @return list of ERs in \link[GenomicRanges]{GRanges} object.
-#' \code{isFisherPass} is \code{TRUE},
+#' \code{isConfirmed} is \code{TRUE},
 #' output is list of confirmed ERs thourgh Fisher's combined test.
 #'
-#' \code{isFisherPass} is \code{FALSE},
+#' \code{isConfirmed} is \code{FALSE},
 #' output is list of discarded ERs due to failing for combined stringency test.
 #' ERs set that failed from Fisher method must be merged with ERs that
 #' failed from minimum overlapping peak requirement, to do so,
@@ -49,13 +49,13 @@
 #'
 #' @export
 #' @importFrom XVector extractList
-#' @importFrom rtracklayer as.data.frame
-#' @importFrom stats setNames
-#' @importFrom BiocGenerics Map
-#' @importFrom data.table setDT
-#' @importFrom data.table .N
-#' @importFrom data.table .I
-#' @importFrom methods hasArg
+# @importFrom rtracklayer as.data.frame
+# @importFrom stats setNames
+# @importFrom BiocGenerics Map
+# @importFrom data.table setDT
+# @importFrom data.table .N
+# @importFrom data.table .I
+# @importFrom methods hasArg
 #' @author Julaiti Shayiding
 #'
 #' @examples
@@ -85,49 +85,36 @@
 #' cmbstrgThreshold=1.0E-08, isFisherPass = FALSE)
 #'
 
-filterBycombStringency <- function(ERs,.hitList,
-                                   cmbstrgThreshold=1.0E-08,
-                                   isFisherPass=TRUE){
-    # input param checking
-    if(!hasArg(ERs)) {
-        stop("required argument ERs is missing,
-             please choose the set of ERs without noise!")
+filterBycombStringency <- function(ovHit, peakset, cmbStrgThreshold,
+                                   isConfirmed=c(TRUE, FALSE)){
+    # sanity input param checking
+    if(!"comb.pv" %in% colnames(ovHit)) {
+        stop("required column is missing")
     }
-    if(!hasArg(.hitList)) {
-        stop("please choose the set of list of overlap hit index for ERs
-             that comply minimum overlapping peak requirement!")
-    }
-    if(!hasArg(cmbstrgThreshold)) {
-        stop("combined stringency threshold must be specified")
-    }
-    stopifnot(is.numeric(cmbstrgThreshold))
-    comb.p <- Fisher_stats(.hitList, ERs)
-    if(isFisherPass==TRUE) {
-        .filtHelper <- function(ele_) {
-            keepMe <- sapply(comb.p, function(x) x<=cmbstrgThreshold)
-            res <- ele_[keepMe]
-        }
-    } else if(isFisherPass==FALSE) {
-        .filtHelper <- function(ele_) {
-            drop_ <- sapply(comb.p, function(x) x > cmbstrgThreshold)
-            res <- ele_[drop_]
-        }
-    }
-    .hitIdx <- lapply(.hitList, .filtHelper)
-    .expandAsGR <- Map(unlist,
-                       mapply(extractList, ERs, .hitIdx))
-    DF <- lapply(.expandAsGR, as.data.frame)
+    stopifnot(class(peakset)=="GRangesList")
+    stopifnot(is.numeric(cmbStrgThreshold))
+    confIdx <- ovHit$comb.pv <= cmbStrgThreshold
+    if(!isConfirmed)
+        res <- ovHit[!confIdx,]
+    res <- ovHit[confIdx,]
+    asGRs <- unlist(extractList(unlist(peakset, use.names = TRUE),
+                                DF$subject), use.names = FALSE)
+
+    rslt <- split(asGRs, names(asGRs))  # FIXME : redudant name in GRanges object
+    return(rslt)
+    #
     # conditional duplicate remobal for each sample
+    # res <- Map(function(x,y)
+    #     setDT(x)[x[, .I[(1:.N) <= y] ,
+    #                .(name, score, p.value)]$V1], DF, 1:length(DF))
+    # asGR <- lapply(res, function(x) as(x, "GRanges"))
+    # rslt <- setNames(asGR, names(ERs))
+    # return (rslt)
+    ##
     # res <- Map(function(x,y)
     #     setDT(x)[x[,
     #                .I[
     #                    if(.N > y) seq_len(pmax(y-1, 1))
     #                    else seq_len(.N)],
     #                .(name, score, p.value)]$V1],DF, 1:length(DF))
-    res <- Map(function(x,y)
-        setDT(x)[x[, .I[(1:.N) <= y] ,
-                   .(name, score, p.value)]$V1], DF, 1:length(DF))
-    asGR <- lapply(res, function(x) as(x, "GRanges"))
-    rslt <- setNames(asGR, names(ERs))
-    return (rslt)
 }
